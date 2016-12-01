326bda5fac6adedcc76a3a3f40d3a3883d618f15
LagacheLouis <louislagache@gmail.com>
2016-12-01 22:03:55 +0100

    architecture

--- site/js/manip2.js
+++
@@ -1,8 +0,0 @@
-$( ".perso" ).click(function() {
-	console.log($(this).attr("id"));
-	var id = $(this).attr("id");
-    $.each(obj.personnages, function (key, val) {
-    	if (key == id)
-    		console.log(val.message);
-    });
-});
