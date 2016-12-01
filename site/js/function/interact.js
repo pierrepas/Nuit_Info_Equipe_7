f2a07254891f8c6476bc075068e65aebfe07f5b5
PierrePas <pierrepas@yahoo.fr>
2016-12-01 22:58:00 +0100

    C'est bon

--- site/js/function/interact.js
+++ site/js/function/interact.js
@@ -3,9 +3,6 @@ $( ".perso" ).click(function() {
 	var id = $(this).attr("id");
     $.each(obj.personnages, function (key, val) {
     	if (key == id)
-
-    		$("#dialog").empty();
-    		$("#dialog").append("<p id='textdialog'>"+val.message+"</p>");
     		console.log(val.message);
     });
 });
